fb602c8da4d0bc653523d522d54ae992fa54a5d3
box-jeon <utrpanic@gmail.com>
2022-07-25 01:31:26 +0900

    Essential Developer. iOS Architecture crash course 3. Extract CardAPI logic into composition adapter.

--- essential-developer/ios-architect-crash-course/iACC/MainTabBarController.swift
+++ essential-developer/ios-architect-crash-course/iACC/MainTabBarController.swift
@@ -87,6 +87,15 @@ class MainTabBarController: UITabBarController {
 	private func makeCardsList() -> ListViewController {
 		let vc = ListViewController()
 		vc.fromCardsScreen = true
+    vc.shouldRetry = false
+    vc.title = "Cards"
+    vc.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: vc, action: #selector(addCard))
+    vc.service = CardAPIItemsServiceAdapter(
+      api: CardAPI.shared,
+      select: { [weak vc] item in
+        vc?.select(card: item)
+      }
+    )
 		return vc
 	}
 	
@@ -119,5 +128,25 @@ struct FriendsAPIItemsServiceAdapter: ItemsService {
 class NullFriendsCache: FriendsCache {
   override func save(_ newFriends: [Friend]) {
     // Do nothing.
+  }
+}
+
+struct CardAPIItemsServiceAdapter: ItemsService {
+  
+  let api: CardAPI
+  let select: (Card) -> Void
+  
+  func loadItems(completion: @escaping (Result<[ItemViewModel], Error>) -> Void) {
+    api.loadCards { result in
+      DispatchQueue.mainAsyncIfNeeded {
+        completion(result.map { items in
+          items.map { item in
+            ItemViewModel(card: item, selection: {
+              select(item)
+            })
+          }
+        })
+      }
+    }
   }
 }
